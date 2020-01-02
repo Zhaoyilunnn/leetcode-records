786d8d74bd445a2598ec5e20c493842e980e347f
Zhaoyilunnn <40353317+Zhaoyilunnn@users.noreply.github.com>
2020-01-02 17:58:09 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/leetcode_record.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode_record.dir/main.cpp.o"
+  "leetcode_record.exe"
+  "leetcode_record.pdb"
+  "libleetcode_record.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode_record.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
